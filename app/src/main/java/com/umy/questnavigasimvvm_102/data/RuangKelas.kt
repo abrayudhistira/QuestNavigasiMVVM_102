dfa30805f0a577f906389be05297305187e478b8
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-11-21 18:39:53 +0700

    membuat model Ruang Kelas

--- app/src/main/java/com/umy/questnavigasimvvm_102/data/RuangKelas.kt
+++ app/src/main/java/com/umy/questnavigasimvvm_102/data/RuangKelas.kt
@@ -1,4 +1,10 @@
 package com.umy.questnavigasimvvm_102.data
-
+//membuat model Ruang Kelas
 object RuangKelas {
+    val kelas = listOf(
+        "A",
+        "B",
+        "C",
+        "D",
+    )
 }
