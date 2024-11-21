123aabcc5f97908da6ac0c109dbb551351b2e123
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-11-21 18:45:24 +0700

    membuat model Mahasiswa dan RencanaStudi

---
+++ app/src/main/java/com/umy/questnavigasimvvm_102/model/Mahasiswa.kt
@@ -0,0 +1,7 @@
+package com.umy.questnavigasimvvm_102.model
+// Model Untuk Mahasiswa
+data class Mahasiswa(
+    val nim: String = "",
+    val nama: String = "",
+    val email: String = ""
+)
---
+++ app/src/main/java/com/umy/questnavigasimvvm_102/model/RencanaStudi.kt
@@ -0,0 +1,6 @@
+package com.umy.questnavigasimvvm_102.model
+// Model untuk Rencana Studi
+data class RencanaStudi(
+    val namaMK: String = "",
+    val kelas: String = ""
+)
