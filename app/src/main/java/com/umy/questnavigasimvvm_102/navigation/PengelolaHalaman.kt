20e1b857751135dc2123131622451bf2fe2ae752
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-11-21 19:44:27 +0700

    Pembenahan Error Code

--- app/src/main/java/com/umy/questnavigasimvvm_102/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/umy/questnavigasimvvm_102/navigation/PengelolaHalaman.kt
@@ -2,16 +2,28 @@ package com.umy.questnavigasimvvm_102.navigation
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.umy.questnavigasimvvm_102.ui.view.screen.MahasiswaFormView
+import com.umy.questnavigasimvvm_102.ui.view.screen.RencanaStudyView
+import com.umy.questnavigasimvvm_102.ui.view.screen.SplashView
+import com.umy.questnavigasimvvm_102.ui.view.screen.TampilView
+import com.umy.questnavigasimvvm_102.ui.view.viewmodel.MahasiswaViewModel
+import com.umy.questnavigasimvvm_102.ui.view.viewmodel.RencanaStudyViewModel
 
-//Deklarasi Variabel
-enum class PengelolaHalaman {
+enum class Halaman {
     Splash,
     Mahasiswa,
     Peminatan,
     TampilKrs
 }
-//Pembuatan Main Program
+
 @Composable
 fun MahasiswaApp(
     modifier: Modifier = Modifier,
@@ -73,3 +85,5 @@ fun MahasiswaApp(
 
     }
 }
+
+//Pembenahan Error
