30d947f767b3a88886be5accbabb4f1ed328fd85
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-11-21 19:54:44 +0700

    Pembenahan TargetSdk menjadi 35 karena dependency lifecycle harus menggunakan TargetSdk menjadi 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.umy.questnavigasimvvm_102"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.umy.questnavigasimvvm_102"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
